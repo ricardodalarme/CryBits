071e7d23022160df45762e7244c3f81bbf785f7f
ricardodalarme <ricardoweasley@hotmail.com>
2021-01-03 09:44:06 -0300

    Changed copyright date

--- Core/Properties/AssemblyInfo.cs
+++ Core/Properties/AssemblyInfo.cs
@@ -7,7 +7,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("Core")]
 [assembly: AssemblyCompany("Ricardo Dalarme")]
 [assembly: AssemblyProduct("CryBits")]
-[assembly: AssemblyCopyright("Copyright (c) 2020 Ricardo Dalarme")]
+[assembly: AssemblyCopyright("Copyright (c) 2021 Ricardo Dalarme")]
 [assembly: AssemblyDescription("ORPG Game Engine")]
 [assembly: AssemblyVersion("0.8.0")]
 
