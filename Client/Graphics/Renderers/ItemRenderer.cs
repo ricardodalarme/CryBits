cf871a1514abe0592b838bc3a4518cddc1dd4960
Ricardo Dalarme <ricardodalarme@outlook.com>
2026-02-28 19:46:09 -0300

    fix: rename item render to item renderer

---
+++ Client/Graphics/Renderers/ItemRenderer.cs
@@ -0,0 +1,25 @@
+using System.Drawing;
+using CryBits.Client.Framework.Graphics;
+using CryBits.Entities;
+using Color = SFML.Graphics.Color;
+
+namespace CryBits.Client.Graphics.Renderers;
+
+internal static class ItemRenderer
+{
+    /// <summary>
+    /// Render an item icon and its amount at the specified slot position.
+    /// </summary>
+    public static void Item(Item item, short amount, Point start, byte slot, byte columns, byte grid = 32,
+      byte gap = 4)
+    {
+        if (item == null) return;
+
+        var line = (slot - 1) / columns;
+        var column = slot - line * 5 - 1;
+        var position = start + new Size(column * (grid + gap), line * (grid + gap));
+
+        Renderer.Instance.Draw(Textures.Items[item.Texture], position);
+        if (amount > 1) Renderer.Instance.DrawText(amount.ToString(), position.X + 2, position.Y + 17, Color.White);
+    }
+}
