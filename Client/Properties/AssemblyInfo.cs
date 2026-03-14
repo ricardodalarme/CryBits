31013fb8574e3847854a84da152ce8fcb87b8b53
Ricardo Dalarme <ricardodalarme@outlook.com>
2026-03-14 16:08:07 -0300

    chore: migrate legacy AssemblyInfo files to .csproj properties

--- Client/Properties/AssemblyInfo.cs
+++
@@ -1,8 +0,0 @@
-using System.Reflection;
-
-[assembly: AssemblyTitle("Client")]
-[assembly: AssemblyCompany("Ricardo Dalarme")]
-[assembly: AssemblyProduct("CryBits")]
-[assembly: AssemblyCopyright("Copyright (c) 2021 Ricardo Dalarme")]
-[assembly: AssemblyDescription("ORPG Game Engine")]
-[assembly: AssemblyVersion("0.8.0")]
