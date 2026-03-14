86aa4203b72cf4af35b06ae9e02107301956dd16
Ricardo Dalarme <ricardodalarme@outlook.com>
2026-03-14 16:08:11 -0300

    refactor: rename AppearanceComponent to FaceComponent

--- Client/Components/Player/AppearanceComponent.cs
+++
@@ -1,7 +0,0 @@
-namespace CryBits.Client.Components.Player;
-
-/// <summary>Visual appearance data for a player character.</summary>
-internal struct AppearanceComponent
-{
-    public short TextureNum;
-}
---
+++ Client/Components/Player/FaceComponent.cs
@@ -0,0 +1,7 @@
+namespace CryBits.Client.Components.Player;
+
+/// <summary>Face data for a player character.</summary>
+internal struct FaceComponent
+{
+    public short TextureNum;
+}
